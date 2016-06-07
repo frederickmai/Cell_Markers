34239871870dcae479df44bebcd2905f0e9e6539
Zifeng Mai <frederickmai@gmail.com>
2016-06-06 23:25:57 -0400

    create static environment

--- cell/urls.py
+++ cell/urls.py
@@ -13,7 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings #Is it settings or cell.settings?
 from django.conf.urls import include, url
+from django.conf.urls.static import static
 from django.contrib import admin
 
 urlpatterns = [
@@ -22,3 +24,6 @@ urlpatterns = [
     # $ means once address is more than panel/, it will not directed to this view
     # url(r'^panel/$', '<appname>.views.<function_name>'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
